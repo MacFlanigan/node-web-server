16f6b450a5006f3da57a66929a825204b775fd31
MacFlanigan <ngl.leclerc@gmail.com>
2017-11-19 10:40:00 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const fs = require('fs');
+
+var app = express();
+const hbs = require('hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method}: ${req.url}`;
+  console.log(log);
+  fs.appendFileSync('server.log', log + '\n', (err) => {
+    console.log('Unable to append to server.log');
+  });
+  next();
+});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home page.',
+    message: 'Welcome to this website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  var d= new Date();
+  d = d.toString();
+  res.send({
+    date: d,
+    errorMessage: 'Unable to complete request'
+  });
+});
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
